833183fec58f9d86d594ca4d8954924cb5e5f654
Danielsm <daniel.smedley@greenmangaming.com>
2012-10-28 20:01:45 +0000

    Stage 2: Status Message and initial menu options.

--- Conflictr/Conflictr.py
+++ Conflictr/Conflictr.py
@@ -2,8 +2,25 @@ import sublime, sublime_plugin
 
 class ConflictrCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		conflicts = self.view.find_all('>{5,}')
-		if len(conflicts) == 1:
+		self.conflicts =self.view.find_all('<{5,}')
+		self.number_of_conflicts = len(self.conflicts)
+		the_message = ("Conflictr has found %d Merge "
+		"Conflicts in this file." % self.number_of_conflicts)
+		self.many_options = ["First Conflict", "Next Conflict", "Previous Conflict"]
+		sublime.status_message(the_message)
+		self.view_index = 0
+		if len(self.conflicts) == 1:
 			self.view.show(conflicts[0])
-		elif len(conflicts) > 1:
-			sublime.Window.show_quick_panel(range(len(conflicts)), self.view.show(conflicts[0]))
+		elif len(self.conflicts) > 1:
+			self.view.window().show_quick_panel(self.many_options, self.scroll_to_conflict)
+		return
+
+	def scroll_to_conflict(self, choice):
+		if choice == -1:
+			return
+		elif choice == 1:
+			self.view.show(self.conflicts[0])
+			self.view_index = 0
+		elif choice == 2:
+			self.view.show(self.conflicts)
+		return
---
+++ Conflictr/conflict_tester.py
@@ -0,0 +1,100 @@
+Some text
+
+
+
+
+
+
+
+some more text
+
+hhege
+
+
+
+
+
+etc etc
+
+
+some code that does stuff
+
+
+
+
+some more code that does stuff
+
+
+
+this is even more code that does stuff
+
+
+
+
+
+
+wooo more code and things.
+
+
+<<<<<<< Updated upstream
+    First conflict!!!!!!!
+=======
+    
+>>>>>>> Stashed changes
+
+
+more text guys
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+so much more text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+woooo text
+
+
+
+
+<<<<<<< Updated upstream
+   Second conflict!!!!!!!
+=======
+    
+>>>>>>> Stashed changes
